ecb816a0689504254b8dc3e28f9e45673dd4da61
Your NameNeirouzJbira <jbira.neirouz@gmail.com>
2021-04-03 03:01:33 +0200

    init

---
+++ src/components/Home.js
@@ -0,0 +1,30 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import { Link } from "react-router-dom";
+import "../App.css";
+
+function Home() {
+  return (
+    <div className="Home">
+      <div className="hero">
+        <div className="hero-text">
+          <h1 className="hero-slogan">
+            The Best of <br></br>TUNISIA For YOU!
+          </h1>
+          <p className="hero-slogan-subtitle">
+            Are you ready for an unforgettable adventure?
+          </p>
+          <Link to="/tours">
+            <button className="contact-btn hero-slogan-contact-btn">
+              Book Tour
+            </button>
+          </Link>
+          <button className="contact-btn hero-slogan-book-btn">Contact</button>
+        </div>
+      </div>
+      <Navbar />
+    </div>
+  );
+}
+
+export default Home;
